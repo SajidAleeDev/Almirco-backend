2f1787c4c91ca8f18a502a5bdff1b192eaa77d3c
Sajid_Alee <pumshuali@gmail.com>
2023-11-05 08:02:29 -0800

    fix issues

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-export { default as User } from "./User.js";
+export { default as User } from "./Users.js";
 export { default as RefreshToken } from "./refreshToken.js";
 export { default as Product } from "./product.js";
 
